ecff73dc1c68c78b7f69283816faf6a251c4e8f5
Laxman Kale <lakhankale888@gmail.com>
2025-11-08 12:28:09 +0530

    Initial portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-purple-600 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">Tailwind is Working ✅</h1>
+    </div>
+  );
+}
