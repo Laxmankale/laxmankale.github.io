d2679037ccf9d42083e3ddc345bc01b27ebff13c
Laxman Kale <lakhankale888@gmail.com>
2025-11-11 19:53:18 +0530

    Fixed App.jsx import structure and connected About & Education sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Education from "./pages/Education";
-
-function App() {
 import Hero from "./pages/Home";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
 import Experience from "./pages/Experience";
+import Education from "./pages/Education";
 import Projects from "./pages/Projects";
 
 export default function App() {
@@ -15,9 +11,9 @@ export default function App() {
     <div className="font-sans bg-gray-900 text-gray-100 min-h-screen">
       <Hero />
       <About />
-      <Education />
       <Skills />
       <Experience />
+      <Education />
       <Projects />
     </div>
   );
